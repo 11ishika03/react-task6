d968a17f105ab24405454ee3b1b877c175b65c9b
Ishika Khandelwal <khandelwalishika09@gmail.com>
2023-11-10 16:43:49 +0530

    first commit

---
+++ src/Aboutme.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Aboutme = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        Hi there! I'm, a dedicated and enthusiastic web developer.I love bringing creativity,
+        attention to detail, and a passion for crafting seamless user experiences.
+      
+      </p>
+      
+    </section>
+  );
+};
+
+export default Aboutme;
